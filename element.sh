7090b14d32dfc2cfb134259b2b4d4b2bc57527d5
duha2060 <maksimov2060@gmail.com>
2023-02-06 09:25:16 +0000

    feat: add scripts

--- element.sh
+++ element.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
-PSQL="psql username=freecodecamp --dbname=periodic_table --no-align --typles-only -c"
-
-if [[ $1 ]]
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
+if [[ -z  $1 ]]
   then
-  #if [[ !$1 =~ ^[0-9]+$ ]]
-  #hen 
-
-  #fi
   echo "Please provide an element as an argument."
 fi 
+
+if [[ $1 =~ ^[0-9]+$ ]]
+  then 
+  element=$($PSQL " select * from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
+else
+  element=$($PSQL " select * from elements join properties using(atomic_number) join types using(type_id) where name = '$1' or symbol = '$1' ")
+ fi
