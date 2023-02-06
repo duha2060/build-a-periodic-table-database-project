1732cd3722859c36cd265fe323a9fba56fb2de49
duha2060 <maksimov2060@gmail.com>
2023-02-06 09:12:45 +0000

    feat:add info

--- element.sh
+++ element.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
 PSQL="psql username=freecodecamp --dbname=periodic_table --no-align --typles-only -c"
+
+if [[ $1 ]]
+  then
+  #if [[ !$1 =~ ^[0-9]+$ ]]
+  #hen 
+
+  #fi
+  echo "Please provide an element as an argument."
+fi 
